66dc622a12f3e0d1408077618984d054b7827096
MatheusSaleh <matheusxmg2@gmail.com>
2023-06-10 20:29:05 -0300

    Implementado um sistema de login
    Aplicativo conectado com o firebase com as devidas adicoes nas dependencias e adicionado um sistema de login para o aplicativo

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,39 @@
-import 'package:appbancodesangue/guia_doacao_sangue.dart';
 import 'package:flutter/material.dart';
+import 'package:appbancodesangue/guia_doacao_sangue.dart';
 import 'package:appbancodesangue/registro_doacao.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
+class AuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<UserCredential?> signInWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      return await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  bool isUserLoggedIn(){
+    return _firebaseAuth.currentUser != null;
+  }
+}
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +49,83 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final AuthService _authService = AuthService();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            TextFormField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: 'Senha'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+                onPressed: () {
+                  final email = _emailController.text;
+                  final password = _passwordController.text;
+                  _loginWithEmailAndPassword(context, email, password);
+                },
+                child: const Text('Login'))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _loginWithEmailAndPassword(BuildContext context, String email, String password) async {
+    final userCredential = await _authService.signInWithEmailAndPassword(email, password);
+    if (userCredential != null) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+      );
+    } else {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Erro'),
+              content: const Text('Falha ao fazer login. Verifique suas credenciais'),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('OK'))
+              ],
+            );
+          });
+    }
+  }
+}
+
 class HomeScreen extends StatelessWidget {
+
+  final AuthService _authService = AuthService();
+
   final List<String> _options = [
     'Notícias',
     'Registrar Doação de Sangue',
@@ -42,10 +144,15 @@ class HomeScreen extends StatelessWidget {
     Icons.admin_panel_settings
   ];
 
-  HomeScreen({super.key});
+  HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
+    if(!_authService.isUserLoggedIn()){
+      return LoginPage();
+    }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -62,12 +169,16 @@ class HomeScreen extends StatelessWidget {
           final option = entry.value;
           return GestureDetector(
             onTap: () {
-              if(index == 2){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => GuiaDoacaoScreen()),
+              if (index == 2) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => GuiaDoacaoScreen()),
                 );
               }
-              if(index == 1){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => FormularioDoacao()),
+              if (index == 1) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FormularioDoacao()),
                 );
               }
             },
--- lib/registro_doacao.dart
+++ lib/registro_doacao.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class FormularioDoacao extends StatefulWidget{
   @override
   _FormularioDoacaoState createState() => _FormularioDoacaoState();
